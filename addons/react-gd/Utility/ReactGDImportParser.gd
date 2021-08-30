22eebf5dae16cbbbcb51ad481e186191cef60dcc
GHSoares <ghsoares99795@gmail.com>
2021-08-30 09:56:52 -0300

    Fixed import preprocessor

--- addons/react-gd/Utility/ReactGDImportParser.gd
+++ addons/react-gd/Utility/ReactGDImportParser.gd
@@ -11,41 +11,45 @@ func parse(code: String) -> String:
 	tokenizer.add_token("symbol", '[\\w.:]+')
 	tokenizer.add_token("string", '"[^"]*"')
 	
-	var tokenized := tokenizer.tokenize(code)
-	
-	var i := 0
-	while i < tokenized.num_tokens:
-		if tokenized.get_token(i).string == "import":
-			assert(
-				tokenized.get_token(i + 1).name == "string",
-				"Expected a string path at line " + str(tokenized.get_token(i).line)
-			)
-			assert(
-				tokenized.get_token(i + 2).string == "as",
-				"Expected 'as' symbol at line " + str(tokenized.get_token(i).line)
-			)
-			assert(
-				tokenized.get_token(i + 3).name == "symbol",
-				"Expected the imported class name " + str(tokenized.get_token(i).line)
-			)
-			
-			var substr := tokenized.slice_string(
-				code, i, i + 3
-			)
-			
-			var path: String = tokenized.get_token(i + 1).string
-			var name: String = tokenized.get_token(i + 3).string
-			path = path.substr(1, path.length() - 2)
-			
-			path = ReactGDPathUtility.global_path(path, source_path)
-			
-			var import_code := 'var ' + name + ' = load("' + path + '")'
-			
-			code = code.replace(substr, import_code)
-			
-			i += 4
-		else:
-			i += 1
+	while true:
+		var tokenized := tokenizer.tokenize(code)
+		
+		var parsed := false
+		var i := 0
+		while i < tokenized.num_tokens:
+			if tokenized.get_token(i).string == "import":
+				assert(
+					tokenized.get_token(i + 1).name == "string",
+					"Expected a string path at line " + str(tokenized.get_token(i).line)
+				)
+				assert(
+					tokenized.get_token(i + 2).string == "as",
+					"Expected 'as' symbol at line " + str(tokenized.get_token(i).line)
+				)
+				assert(
+					tokenized.get_token(i + 3).name == "symbol",
+					"Expected the imported class name " + str(tokenized.get_token(i).line)
+				)
+				
+				var substr := tokenized.slice_string(
+					code, i, i + 3
+				)
+				
+				var path: String = tokenized.get_token(i + 1).string
+				var name: String = tokenized.get_token(i + 3).string
+				path = path.substr(1, path.length() - 2)
+				
+				path = ReactGDPathUtility.global_path(path, source_path)
+				
+				var import_code := 'var ' + name + ' = load("' + path + '")'
+				
+				code = code.replace(substr, import_code)
+				
+				parsed = true
+				break
+			else:
+				i += 1
+		if !parsed: break
 	
 	return code
 
