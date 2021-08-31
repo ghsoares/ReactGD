65d1a690565839d35c1d5d83082eeb4be0afc896
GHSoares <ghsoares99795@gmail.com>
2021-08-31 20:18:55 -0300

    Now the tree state difference is calculated on the fly. It makes life easier for me as I can access the values directly

---
+++ addons/react-gd/Utility/ReactGDTreeBuilder.gd
@@ -0,0 +1,274 @@
+extends Node
+
+class_name ReactGDTreeBuilder
+
+# Relevant data from component
+var cached_nodes: Dictionary
+var root_component: Node
+
+"""
+Create node function
+This function takes a node data, current cached nodes dictionary and path.
+cached_nodes is provided by the ReactGDComponent, it stores already created node
+for a id, without the need to remove all the children of the tree and add then
+again.
+"""
+func create_node(node_data: Dictionary, path: String) -> Dictionary:
+	# Grab all the relevant data
+	var node_id: String = node_data.id
+	var node_props: Dictionary = node_data.props
+	var node_children: Dictionary = node_props.children
+	var node_cached_path: String = path + node_id
+	var node_type = node_data.type
+	
+	# Creates the node container
+	var node: Dictionary = {}
+	
+	# This node is new, so add it to cached nodes
+	if not cached_nodes.has(node_cached_path):
+		# First we create the container, creating the instance after the provided type
+		node = {
+			"id": node_id,
+			"instance": node_type.new(),
+			"props": node_props,
+			"cached_path": node_cached_path
+		}
+		# Then store the instance in the cached nodes in the path
+		cached_nodes[node_cached_path] = node.instance
+	# This node exists, so just grab the instance
+	else:
+		node = {
+			"id": node_id,
+			"instance": cached_nodes[node_cached_path],
+			"props": node_props,
+			"cached_path": node_cached_path
+		}
+	
+	# This node has children, so create then too
+	if not node_children.empty():
+		for child_id in node_children.keys():
+			var child_data: Dictionary = node_children[child_id]
+			node_children[child_id] = create_node(child_data, node_cached_path)
+	
+	return node
+
+"""
+Update node function
+This function takes the root component, parent node, instance child index,
+previous node state and current node state.
+The node is then removed if needed, update the props and add it if needed.
+"""
+func update_node(
+	parent_node: Node, idx: int,
+	prev_node_state: Dictionary, node_state: Dictionary
+) -> void:
+	# Both states are empty, abort
+	if prev_node_state.empty() and node_state.empty(): return
+	
+	# Get the previous relevant data.
+	# As previous data may be empty, so we use `get` with default
+	# parameter to avoid errors
+	var prev_instance: Node = prev_node_state.get("instance", null)
+	var prev_props: Dictionary = prev_node_state.get("props", {})
+	var prev_children: Dictionary = prev_props.get("children", {})
+	var prev_instance_is_component: bool = prev_instance and prev_instance.get_class() == "ReactGDComponent"
+	
+	# The node was removed, so just remove it before going further
+	if node_state.empty():
+		# Remove this node from the tree
+		parent_node.remove_child(prev_instance)
+		
+		# Remove it's registry from the cached nodes
+		cached_nodes.erase(prev_node_state.cached_path)
+		
+		# Remove previous children
+		for child_id in prev_children.keys():
+			var child: Dictionary = prev_children[child_id]
+			# If the current instance is a component,
+			# the node must be removed from the instance parent,
+			# else the node must be removed from the instance node
+			if prev_instance_is_component:
+				update_node(parent_node, -1, child, {})
+			else:
+				update_node(prev_instance, -1, child, {})
+		
+		# Queue free it from the free
+		prev_instance.queue_free()
+		return
+	
+	# Get the current relevant data.
+	var current_instance: Node = node_state.instance
+	var current_props: Dictionary = node_state.props
+	var current_children: Dictionary = current_props.children
+	var instance_is_component: bool = current_instance and current_instance.get_class() == "ReactGDComponent"
+	
+	# Update the props
+	var props_changed := update_node_props(
+		root_component, node_state.instance,
+		prev_node_state.get("props", {}), node_state.props
+	)
+	# If the props has changed and the instance node
+	# is a component, query it to render
+	if instance_is_component and props_changed:
+		current_instance._dirty = true
+	
+	# The node was added
+	if prev_node_state.empty():
+		parent_node.add_child(current_instance)
+		parent_node.move_child(current_instance, idx)
+	# This node already exists
+	else:
+		# Update the previous children
+		for child_id in prev_children.keys():
+			var child: Dictionary = prev_children[child_id]
+			# This node was removed
+			if not current_children.has(child_id):
+				# If the current instance is a component,
+				# the node must be removed from the instance parent,
+				# else the node must be removed from the instance node
+				if instance_is_component:
+					update_node(parent_node, -1, child, {})
+				else:
+					update_node(current_instance, -1, child, {})
+	
+	# The node child index, dictates the index position as child of a node
+	var node_idx := 0
+	for child_id in current_children.keys():
+		var child: Dictionary = current_children[child_id]
+		# This node was added
+		if not prev_children.has(child_id):
+			# If the current instance is a component,
+			# the node must be added from the instance parent,
+			# else the node must be added from the instance node
+			if instance_is_component:
+				update_node(parent_node, node_idx, {}, child)
+			else:
+				update_node(current_instance, node_idx, {}, child)
+		else:
+			# If the current instance is a component,
+			# the node must be updated from the instance parent,
+			# else the node must be updated from the instance node
+			if instance_is_component:
+				update_node(parent_node, node_idx, prev_children[child_id], child)
+			else:
+				update_node(current_instance, node_idx, prev_children[child_id], child)
+		
+		# This node is a component, jump a node index, because is reserved to the
+		# component rendered children
+		if child.instance.get_class() == "ReactGDComponent":
+			node_idx += 2
+		# Else just increment the node index
+		else:
+			node_idx += 1
+
+"""
+Update node props function
+This function takes the root component, instance node, previous props and
+current props.
+The prop set behaviour is different to each prop, specially for component's built-ins
+like children, theme, signals, ref, etc.
+"""
+func update_node_props(
+	root_component: Node, node: Node, prev_props: Dictionary,
+	props: Dictionary
+) -> bool:
+	# Tells if the node properties have been changed
+	var changed := false
+	
+	# If the node is a component, just pass the props to the component's
+	# `props` variable
+	if node.get_class() == "ReactGDComponent":
+		for prop_name in props.keys():
+			if not prev_props.has(prop_name) or hash(prev_props[prop_name]) != hash(props[prop_name]):
+				node.props[prop_name] = props[prop_name]
+				changed = true
+		return changed
+	
+	# Pass to each assigned props, signals, themes, ref, etc.
+	for prop_name in props.keys():
+		var prop_value = props[prop_name]
+		
+		# Children is ignored for regular nodes,
+		# even for custom types, use `get_children` instead
+		if prop_name == "children": continue
+		# If the prop name begins with `on_`, this means
+		# that this prop is a signal
+		elif prop_name.begins_with("on_"):
+			var signal_name: String = prop_name.substr(3, prop_name.length() - 3)
+			if not prev_props.has(prop_name):
+				node_update_signal(node, signal_name, null, prop_value)
+			else:
+				node_update_signal(node, signal_name, prev_props[prop_name], prop_value)
+		else:
+			node_update_prop(node, prop_name, prop_value)
+			changed = true
+	
+	return changed
+
+"""
+Update the node property.
+Some nodes have undesired behaviour on properties changes,
+so this function takes care of these behaviours.
+"""
+func node_update_prop(node: Node, prop_name: String, prop_value) -> void:
+	# LineEdit resets the caret position when text is changed
+	if node is LineEdit and prop_name == "text":
+		var before: int = node.caret_position
+		node.text = prop_value
+		node.caret_position = before
+		return
+	
+	# Default behaviour, just set the node property
+	node.set_indexed(prop_name, prop_value)
+
+"""
+Update the node signal.
+Compares the previous connection and disconnect if needed.
+"""
+func node_update_signal(node: Node, signal_name: String, previous_value, current_value) -> void:
+	# There was a connection
+	if previous_value:
+		# Signal connection is different, disconect first
+		if not current_value or typeof(previous_value) != typeof(current_value) or\
+			hash(previous_value) != hash(current_value):
+			
+			if previous_value is String:
+				node.disconnect(signal_name, root_component, previous_value)
+			elif previous_value is Array:
+				node.disconnect(signal_name, root_component, previous_value[0])
+			elif previous_value is Dictionary:
+				node.disconnect(signal_name, root_component, previous_value.target)
+		# Connection is the same, return
+		else: return
+		
+		# There is no new connection, don't connect
+		if not current_value: return
+	
+	# Handle the connection
+	if current_value is String:
+		node.connect(signal_name, root_component, current_value)
+	elif current_value is Array:
+		if current_value.size() == 1:
+			node.connect(signal_name, root_component, current_value[0])
+		elif current_value.size() == 2:
+			node.connect(signal_name, root_component, current_value[0], current_value[1])
+		elif current_value.size() == 3:
+			node.connect(signal_name, root_component, current_value[0], current_value[1], current_value[2])
+	elif current_value is Dictionary:
+		node.connect(
+			signal_name, root_component, current_value.target,
+			current_value.get("binds", []), current_value.get("flags", 0)
+		)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- addons/react-gd/classes/ReactGDComponent.gd
+++ addons/react-gd/classes/ReactGDComponent.gd
@@ -1,484 +1,121 @@
 extends Node
-
 class_name ReactGDComponent
 
-enum DIFF_TYPE {
-	DIFF_ADDED = 0,
-	DIFF_MODIFIED = 1,
-	DIFF_REMOVED = 2,
-	DIFF_UNCHANGED = 3
-}
-
-var _dirty: bool
-var _render_state: Dictionary
+# Some "private" variables, but as Godot don't
+# have access modifiers so it's needed to prefix
+# with `_` to tell this variable is private, so
+# don't touch these.
+# Cached nodes stores the instantiated nodes,
+# so there is no need to re-instantiate then when
+# rendering the same node again
 var _cached_nodes: Dictionary
-var _tw: Tween
-var parent_component: Node
+# Current tree state stores the tree state that can
+# be used to compare difference between two renders
+var _current_tree_state: Dictionary
+# Parent component stores the component that instantiated
+# this component, so in case of root components of a UI this
+# will be null
+var _parent_component: Node
+# Dirty boolean says if the node needs to render again,
+# in case of a prop or state change
+var _dirty: bool
+
+# State variable, store the actual state of the node,
+# can be accessed from custom components
 var state: Dictionary
-var stores: Dictionary
-var events: Dictionary
-var children: Array
+# Props variable, store the props passed from another component,
+# can be accessed from custom components
+var props: Dictionary
 
+"""
+Init function, sets the initial variables
+"""
 func _init() -> void:
-	_render_state = {}
 	_cached_nodes = {}
-	stores = {}
-
-func _ready() -> void:
-	_tw = Tween.new()
-	add_child(_tw)
+	_current_tree_state = {}
 	
 	state = {}
-	events = {}
-	
+	props = {}
+
+"""
+Ready function, calls the component constructor
+"""
+func _ready() -> void:
 	construct()
 
+"""
+Enter tree function, queries the component to render
+"""
 func _enter_tree() -> void:
 	_dirty = true
 
+"""
+Process function, call `_render_process` if need to render
+"""
 func _process(delta: float) -> void:
 	if _dirty:
-		var start := OS.get_ticks_msec()
-		
 		_render_process()
 		_dirty = false
-		
-		var elapsed := OS.get_ticks_msec() - start
-		print("(", name, ") Render: ", elapsed, " ms")
-	
-	for node in _cached_nodes.values():
-		if not node.is_inside_tree(): continue
-		
-		var props: Dictionary = node.get_meta("props")
-		if node is Control:
-			node.rect_pivot_offset = node.rect_size * props.get("rect_pivot_center", Vector2.ZERO)
 
+"""
+Main render process function, will be called when this component needs to update.
+Takes the new render, build the node tree, set the properties and add nodes when needed
+"""
 func _render_process() -> void:
-	var new_render = render()
-	if not new_render: new_render = {}
-	var new_render_state = _build_component(new_render, "")
+	# Uses another class to render the tree 
+	# for readability purposes only
+	var tree_builder := ReactGDTreeBuilder.new()
+	tree_builder.root_component = self
+	tree_builder.cached_nodes = _cached_nodes
 	
-	if new_render_state.hash() != _render_state.hash():
-		var render_diff = ReactGDDictionaryMethods.diff(_render_state, new_render_state)
-		if render_diff.empty(): return
-		if parent_component:
-			_update_tree(render_diff, get_parent(), get_index() + 1)
-		else:
-			_update_tree(render_diff, self, 0)
-		
-		_render_state = new_render_state
+	# Get the new render state
+	var new_render: Dictionary = render()
+	# Get the new tree state builded from the render
+	var tree_state: Dictionary = tree_builder.create_node(new_render, "")
 	
-	for ev in events.keys():
-		events[ev] = false
-
-func _build_component(render_state: Dictionary, path: String) -> Dictionary:
-	if render_state.empty(): return {}
-	var props: Dictionary = render_state.get("props", {})
-	var children: Array = render_state.get("children", [])
-	render_state.id += str(props.get("key", ""))
-	
-	var node :Dictionary = {}
-	var cached_path :String = path + render_state.id
-	
-	if _cached_nodes.has(cached_path):
-		node = {
-			"cached_path": cached_path,
-			"instance": _cached_nodes[cached_path],
-			"signals": {},
-			"theme": {
-				"styles": {},
-				"constants": {}
-			},
-			"ref": ""
-		}
-	else:
-		node = {
-			"cached_path": cached_path,
-			"instance": render_state.type.new(),
-			"signals": {},
-			"theme": {
-				"styles": {},
-				"constants": {}
-			},
-			"ref": ""
-		}
-		_cached_nodes[cached_path] = node.instance
-	
-	var dict_children := {}
-	
-	for prop_name in props:
-		if props[prop_name] is ReactGDTransition:
-			props[prop_name] = {
-				"reactgd_transition_data": {
-					"frames": props[prop_name]._frames,
-					"props": props[prop_name]._props,
-					"hash": props[prop_name]._hash
-				}
-			}
-		elif prop_name == "ref":
-			node.ref = props[prop_name]
-		elif prop_name == "children":
-			children.append_array(props[prop_name])
-		elif prop_name.begins_with("style_"):
-			var style_name = prop_name.substr(6, prop_name.length())
-			node.theme.styles[style_name] = props[prop_name]
-		elif prop_name.begins_with("const_"):
-			var const_name = prop_name.substr(6, prop_name.length())
-			node.theme.constants[const_name] = props[prop_name]
-		elif prop_name.begins_with("on_"):
-			var signal_name = prop_name.substr(3, prop_name.length())
-			if node.instance.has_signal(signal_name):
-				if props[prop_name] is String and props[prop_name] != "":
-					node.signals[signal_name] = props[prop_name]
-	
-	if node.instance.get_class() == "ReactGDComponent":
-		node.instance.children = children
-		node.children = {}
-	else:
-		var prev_conditional_true := false
-		var new_children := {}
-		for child in children:
-			if child.type is String:
-				if child.type == "If":
-					prev_conditional_true = child.props.conditional
-					if prev_conditional_true:
-						for n_child in child.children:
-							new_children[n_child.id] = _build_component(n_child, cached_path + "_")
-				
-				elif child.type == "Elif":
-					if prev_conditional_true: continue
-					prev_conditional_true = child.props.conditional
-					if prev_conditional_true:
-						for n_child in child.children:
-							new_children[n_child.id] = _build_component(n_child, cached_path + "_")
-				
-				elif child.type == "Else":
-					if prev_conditional_true: continue
-					for n_child in child.children:
-						new_children[n_child.id] = _build_component(n_child, cached_path + "_")
-			else:
-				new_children[child.id] = _build_component(child, cached_path + "_")
-		node.children = new_children
-	
-	node.instance.set_meta("props", props)
-	
-	node.props = props
-	return node
-
-func _update_tree(render_diff: Dictionary, parent: Node, index: int) -> void:
-	var cached_path: String = render_diff.cached_path.value
-	var node_change_type: int = render_diff.instance.change_type
-	var node: Node = render_diff.instance.value
-	var ref: String = render_diff.ref.value
-	
-	if node_change_type != DIFF_TYPE.DIFF_REMOVED:
-		var props_change_type: int = render_diff.props.change_type
-		var props: Dictionary = render_diff.props.value
-		
-		var signals_change_type: int = render_diff.signals.change_type
-		var signals: Dictionary = render_diff.signals.value
-		
-		var theme_change_type: int = render_diff.theme.change_type
-		var theme: Dictionary = render_diff.theme.value
-		
-		if props_change_type != DIFF_TYPE.DIFF_UNCHANGED:
-			_update_node_props(node, props)
-			if node.get_class() == "ReactGDComponent":
-				node._dirty = true
-		
-		if signals_change_type != DIFF_TYPE.DIFF_UNCHANGED:
-			_update_node_signals(node, signals)
-		
-		if theme_change_type != DIFF_TYPE.DIFF_UNCHANGED:
-			_update_node_theme(node, theme)
-	
-	if node_change_type == DIFF_TYPE.DIFF_ADDED:
-		if node.get_class() == "ReactGDComponent":
-			node.parent_component = self
-			for store_name in stores.keys():
-				node.stores[store_name] = stores[store_name]
-		parent.add_child(node, true)
-		parent.move_child(node, index)
-		if node.get_class() == "ReactGDComponent":
-			node._render_process()
-		if ref != "":
-			self.set_indexed(ref, node)
-	
-	elif node_change_type == DIFF_TYPE.DIFF_REMOVED:
-		parent.remove_child(node)
-		if node.get_class() == "ReactGDComponent":
-			parent.remove_child(node._render_state.instance)
-		
-		_cached_nodes.erase(cached_path)
-		node.queue_free()
-		if node.get_class() == "ReactGDComponent":
-			node._render_state.instance.queue_free()
-		if ref != "":
-			self.set_indexed(ref, null)
-		return
-	
-	var children_change_type: int = render_diff.children.change_type
-	var children: Dictionary = render_diff.children.value
-	
-	if children_change_type != DIFF_TYPE.DIFF_UNCHANGED:
-		var child_index := 0
-		for c in children.keys():
-			var child = children[c]
-			
-			if child.change_type != DIFF_TYPE.DIFF_UNCHANGED:
-				_update_tree(child.value, node, child_index)
-				if child.value.instance.value.get_class() == "ReactGDComponent":
-					child_index += 1
-			else:
-				if child.value.instance.get_class() == "ReactGDComponent":
-					child_index += 1
-			
-			child_index += 1
-
-func _update_node_props(node: Node, props: Dictionary) -> void:
-	for prop_name in props.keys():
-		if props[prop_name].change_type == DIFF_TYPE.DIFF_UNCHANGED: continue
-		var prop_value = props[prop_name].value
-		if prop_value is Dictionary and prop_value.has("reactgd_transition_data"):
-			_update_transition(node, prop_name, prop_value["reactgd_transition_data"])
-			continue
-		node.set_indexed(prop_name, prop_value)
-		if node is LineEdit:
-			if prop_name == "text":
-				node.caret_position = prop_value.length()
-
-func _update_node_signals(node: Node, signals: Dictionary) -> void:
-	for signal_name in signals.keys():
-		if signals[signal_name].change_type == DIFF_TYPE.DIFF_UNCHANGED: continue
-		_update_node_signal(node, signal_name, signals[signal_name])
-
-func _update_node_theme(node: Control, theme: Dictionary) -> void:
-	#print(theme)
-	if theme.styles.change_type != DIFF_TYPE.DIFF_UNCHANGED:
-		var styles: Dictionary = theme.styles.value
-		for style_name in styles.keys():
-			if styles[style_name].change_type == DIFF_TYPE.DIFF_UNCHANGED: continue
-			if styles[style_name].change_type == DIFF_TYPE.DIFF_REMOVED:
-				node.add_stylebox_override(style_name, null)
-				continue
-			var style = styles[style_name].value
-			if style == null: continue
-			if style.empty():
-				node.add_stylebox_override(style_name, StyleBoxEmpty.new())
-				continue
-			
-			var stylebox: StyleBox = null
-			
-			if styles[style_name].change_type == DIFF_TYPE.DIFF_ADDED:
-				stylebox = style.type.value.new()
-				node.add_stylebox_override(style_name, stylebox)
-			else:
-				stylebox = node.get_stylebox(style_name)
-			
-			var props: Dictionary = style.get("props", {}).get("value", {})
-			for prop_name in props.keys():
-				if props[prop_name].change_type == DIFF_TYPE.DIFF_REMOVED:
-					stylebox.set_indexed(
-						prop_name, style.type.value.new().get_indexed(prop_name)
-					)
-				else:
-					var prop_val = props[prop_name].value
-					if prop_val is Dictionary and prop_val.has("reactgd_transition_data"):
-						_update_transition(stylebox, prop_name, prop_val["reactgd_transition_data"])
-						continue
-					
-					stylebox.set_indexed(prop_name, prop_val)
-	if theme.constants.change_type != DIFF_TYPE.DIFF_UNCHANGED:
-		var constants: Dictionary = theme.constants.value
-		for const_name in constants.keys():
-			node.add_constant_override(const_name, constants[const_name].value)
-
-func _update_transition(obj: Object, prop_name: String, transition_data: Dictionary) -> void:
-	if transition_data.change_type == DIFF_TYPE.DIFF_REMOVED: return
-	
-	var frames: Array = transition_data.value.frames.value
-	var prop_names: Array = transition_data.value.props.value
-	var prop_values: Dictionary = {}
-	
-	if prop_name == "transition":
-		prop_name = ""
-	else:
-		_tw.stop(obj, prop_name)
-		prop_values = {
-			prop_name: obj.get_indexed(prop_name)
-		}
-	
-	for p_name in prop_names:
-		if p_name != "":
-			prop_values[prop_name + p_name] = obj.get_indexed(prop_name + p_name)
-			_tw.stop(obj, prop_name + p_name)
-	
-	frames.sort_custom(ReactGDTransition, "sort_frames")
-	
-	for frame in frames:
-		var p_name: String = prop_name + frame.prop
-		var start_value = prop_values[p_name]
-		var final_value = frame.final_value
-		var time: float = frame.time
-		var duration: float = frame.duration
-		var trans_type: int = frame.trans_type
-		var ease_type: int = frame.ease_type
-		
-		_tw.interpolate_property(
-			obj, p_name, start_value, final_value, duration, trans_type, ease_type,
-			time
+	if _parent_component == null:
+		# This is the root component, so render the nodes as children of this component
+		tree_builder.update_node(
+			 self, 0,
+			_current_tree_state, tree_state
 		)
-		
-		prop_values[p_name] = final_value
-	
-	_tw.start()
-
-func _update_node_signal(node: Node, signal_name: String, signal_data: Dictionary) -> void:
-	var target_node: Node = self
-	var target_function: String
-	var binds: Array
-	var flags: int
-	
-	if signal_data.value is String:
-		target_function = signal_data.value
-		binds = []
-		flags = 0
-	elif signal_data.value is Array:
-		var args_size: int = signal_data.value.size()
-		if args_size == 1:
-			target_function = signal_data.value[0]
-			binds = []
-			flags = 0
-		elif args_size == 2:
-			target_function = signal_data.value[0]
-			binds = signal_data.value[1]
-			flags = 0
-		elif args_size == 3:
-			target_function = signal_data.value[0]
-			binds = signal_data.value[1]
-			flags = signal_data.value[2]
-	
-	if signal_data.change_type == DIFF_TYPE.DIFF_ADDED:
-		node.connect(signal_name, target_node, target_function, binds, flags)
-	elif signal_data.change_type == DIFF_TYPE.DIFF_MODIFIED:
-		node.disconnect(signal_name, target_node, target_function)
-		node.connect(signal_name, target_node, target_function, binds, flags)
-	elif signal_data.change_type == DIFF_TYPE.DIFF_REMOVED:
-		node.disconnect(signal_name, target_node, target_function)
-
-func _on_store_changed() -> void:
-	self._dirty = true
-
-func do_transition() -> ReactGDTransition:
-	return ReactGDTransition.new()
-
-func create_style(style_data) -> Dictionary:
-	var data := {}
-	if style_data is Array:
-		for d in style_data:
-			ReactGDDictionaryMethods.merge_dict(data, d)
-	elif style_data is Dictionary:
-		data = style_data
 	else:
-		assert(false, "Invalid style data type")
-	assert(data.has("type"), "Style must have the stylebox type!")
-	var style := {
-		"type": data.type,
-		"props": {}
-	}
+		# This component was added by a parent component, so render the nodes as children
+		# of the parent node with the index being the index of this component + 1.
+		# The reason why is that if we add these nodes as children of this component,
+		# but the parent node is a container, the container will not affect the rendered
+		# nodes, so we add then as children of the container instead.
+		tree_builder.update_node(
+			get_parent(), get_index() + 1,
+			_current_tree_state, tree_state
+		)
 	
-	for prop_name in data.keys():
-		if prop_name == "type": continue
-		var prop_value = data[prop_name]
-		
-		if prop_value is ReactGDTransition:
-			prop_value = {
-				"reactgd_transition_data": {
-					"frames": prop_value._frames,
-					"props": prop_value._props,
-					"hash": prop_value._hash
-				}
-			}
-		
-		match prop_name:
-			"border_width":
-				for p in ["_left", "_right", "_top", "_bottom"]:
-					style.props["border_width" + p] = prop_value
-			"border_width_horizontal":
-				for p in ["_left", "_right"]:
-					style.props["border_width" + p] = prop_value
-			"border_width_vertical":
-				for p in ["_top", "_bottom"]:
-					style.props["border_width" + p] = prop_value
-			"corner_radius":
-				for p in ["_top_left", "_top_right", "_bottom_left", "_bottom_right"]:
-					style.props["corner_radius" + p] = prop_value
-			"expand_margin":
-				for p in ["_left", "_right", "_top", "_bottom"]:
-					style.props["expand_margin" + p] = prop_value
-			"expand_margin_horizontal":
-				for p in ["_left", "_right"]:
-					style.props["expand_margin" + p] = prop_value
-			"expand_margin_vertical":
-				for p in ["_top", "_bottom"]:
-					style.props["expand_margin" + p] = prop_value
-			"content_margin":
-				for p in ["_left", "_right", "_top", "_bottom"]:
-					style.props["content_margin" + p] = prop_value
-			"content_margin_horizontal":
-				for p in ["_left", "_right"]:
-					style.props["content_margin" + p] = prop_value
-			"content_margin_vertical":
-				for p in ["_top", "_bottom"]:
-					style.props["content_margin" + p] = prop_value
-			_:
-				style.props[prop_name] = prop_value
-	
-	return style
+	# Set the current tree state as the new tree state
+	_current_tree_state = tree_state
 
-func create_store(reducer: String) -> ReactGDStore:
-	return ReactGDStore.new(funcref(self, reducer))
-
-func register_store(store_name: String, store: ReactGDStore) -> void:
-	stores[store_name] = store
-
-func subscribe_to_store(store_name: String) -> void:
-	stores[store_name].subscribe(self, "_on_store_changed")
-
-func unsubscribe_from_store(store_name: String) -> void:
-	stores[store_name].unsubscribe(self, "_on_store_changed")
-
-func dispatch_action(store_name: String, action_type: String, payload) -> void:
-	stores[store_name].dispatch(action_type, payload)
-
-func get_store_state(store_name: String):
-	return stores[store_name].state
-
+"""
+Set state function, called when the component want to change the current state.
+Sets only the provided keys of the state and queries the component to render.
+"""
 func set_state(new_state: Dictionary) -> void:
-	for state_key in new_state.keys():
-		state[state_key] = new_state[state_key]
+	for key in new_state.keys():
+		state[key] = new_state[key]
 	_dirty = true
 
-func trigger_event(event_name: String, val: bool = true) -> void:
-	events[event_name] = val
-	_dirty = true
-
-func add_child(node, legible_name = false):
-	.add_child(node, legible_name)
-
+"""
+Component constructor function, called on ready
+"""
 func construct() -> void: pass
 
+"""
+Main render function, will be overrided by other component scripts
+"""
 func render() -> Dictionary: return {}
 
-func get_class() -> String: return "ReactGDComponent"
-
-
-
-
-
-
-
-
+"""
+get_string override function, returns class_name
+"""
+func get_string() -> String: return "ReactGDComponent"
 
 
 
