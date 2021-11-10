b0a4e6068ed805b87cb04c44ee1ae10e845d2503
ghsoares <ghsoares99795@gmail.com>
2021-11-10 17:15:18 -0300

    .

--- language/test/main.cpp
+++ language/test/main.cpp
@@ -21,7 +21,7 @@ int main() {
 	try {
 		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 		
-		parser->parse(source);
+		parser->parse(source, "res://scripts/");
 
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 		auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
