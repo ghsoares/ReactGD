f24d0fed0f643462fe1368f933d876e0815192e8
Gabriel <ghsoares99795@gmail.com>
2022-03-24 19:18:50 -0300

    Another version

---
+++ addons/ReactGD/plugin.gd
@@ -0,0 +1,12 @@
+tool
+extends EditorPlugin
+
+# Called when the node enters the tree
+func _enter_tree() -> void:
+	# Add ReactGD singleton
+	add_autoload_singleton("ReactGD", "res://addons/ReactGD/singleton/ReactGD.gd")
+
+# Called when the node exits the tree
+func _exit_tree() -> void:
+	# Remove ReactGD singleton
+	remove_autoload_singleton("ReactGD")
